d226dad93f1b0f637161d5c1e720900d0f48418f
Iddar Olivares <iddar.olivares.servicios@gmail.com>
2014-10-04 02:15:06 -0500

    Agrego una pequeña bateria de pruebas para los sockets y el servidor de express

---
+++ test/joystick-test/joystick-test.ino
@@ -0,0 +1,34 @@
+int pot1;
+int pot2;
+
+int pot1Last = 0;
+int pot2Last = 0;
+
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(0, player1, RISING);
+  attachInterrupt(1, player2, RISING);
+}
+ 
+void loop() { 
+  pot1 = map(analogRead(A0),0,1023,0,100);
+  pot2 = map(analogRead(A1),0,1023,0,100);
+  if(pot1 != pot1Last || pot2 != pot2Last){
+    Serial.print("{\"analog\":[");
+    Serial.print( pot1 );
+    Serial.print(",");
+    Serial.print( pot2 );
+    Serial.print("]}\n");
+    pot1Last = pot1;
+    pot2Last = pot2;
+  }
+  delay(10);
+}
+
+void player1() {
+  Serial.print("{\"player\": 0 }\n");
+}
+
+void player2(){
+  Serial.print("{\"player\": 1 }\n");
+}
---
+++ test/pong-test.js
@@ -0,0 +1,50 @@
+var URL = 'http://0.0.0.0:3000';
+
+var should = require('chai').should();
+var io = require('socket.io-client');
+var supertest = require('supertest');
+var api = supertest(URL);
+
+var options ={
+  transports: ['websocket'],
+  'force new connection': true
+};
+
+
+describe("Socket Server",function(){
+  var client = io.connect(URL, options);
+
+  it('Get player buttom press',function(){
+
+    client.on('button',function(data){
+      data.should.be.a('number');
+      client.disconnect();
+    });
+
+  });
+
+  it('Get joystick values', function () {
+
+    client.on('button',function(position){
+      position.to.eql([ 0, 1023 ]);
+      client.disconnect();
+    });
+
+  });
+
+});
+
+
+describe('Get a home page', function() {
+
+  it('Print index page', function(done) {
+    api.get('/')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+
+});
