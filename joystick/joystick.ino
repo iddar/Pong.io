7877695f86403b677155c597a423a3945150c172
gorrotowi <t_o_wi@hotmail.com>
2014-10-06 18:31:27 -0500

    BugLecture
    Lectura del potenciometro, si hay una diferencia significativa se envía
    el dato serial evitando el “rebote” de las barras del juego

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -15,13 +15,15 @@ void loop() {
   pot1 = map(analogRead(A0),1023,820,0,100);
   pot2 = map(analogRead(A1),815,1023,0,100);
   if(pot1 != pot1Last || pot2 != pot2Last){
-    Serial.print("{\"analog\":[");
-    Serial.print( pot1 );
-    Serial.print(",");
-    Serial.print( pot2 );
-    Serial.print("]}\n");
-    pot1Last = pot1;
-    pot2Last = pot2;
+    if((pot1 > pot1Last + 1 || pot1 < pot1Last - 1) || (pot2 > pot2Last + 1 || pot2 < pot2Last - 1)){
+      Serial.print("{\"analog\":[");
+      Serial.print( pot1 );
+      Serial.print(",");
+      Serial.print( pot2 );
+      Serial.print("]}\n");
+      pot1Last = pot1;
+      pot2Last = pot2;
+    }
   }
   delay(10);
 }
