f2c2058ac8f69a68f6f808f2d3037396423b5993
gorrotowi <t_o_wi@hotmail.com>
2014-10-05 20:41:22 -0500

    Score view
    Agregando titulo de ganador, cuando alguno de los dos jugadores llegue
    a 7 se mostrara un pequeño letrero en la parte inferior mostrando al
    ganador. Cuando el marcador sea 0 - 0 el letrero desaparecerá, seria
    ideal que se obtuviera el nombre o siglas del ganador para poder
    mostrarlo :)

--- static/js/score.js
+++ static/js/score.js
@@ -2,6 +2,18 @@ var socket = io();
 socket.on("showScore",function(score){
     var playerOne = document.querySelector('#PlayerOne');
     var playerTwo = document.querySelector('#PlayerTwo');
+    var winner = document.querySelector('#winner');
     playerOne.innerText = '0' + score.player1;
     playerTwo.innerText = '0' + score.player2;
+
+    if(score.player1 === 0 && score.player2 === 0){
+      document.getElementById("winner").style.display = "none";
+    }
+    if(score.player1 === 7){
+      winner.innerText = 'Player 1 winner';
+      document.getElementById("winner").style.display = "block";
+    }else if(score.player2 === 7){
+      winner.innerText = 'Player 2 winner';
+      document.getElementById("winner").style.display = "block";
+    }
 });
