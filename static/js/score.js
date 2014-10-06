844a3103fef227b7ff9575beab994aa8aba4b7c1
gorrotowi <t_o_wi@hotmail.com>
2014-10-05 20:06:22 -0500

    ScoreScreen
    Score screen view

--- static/js/score.js
+++ static/js/score.js
@@ -1,10 +1,7 @@
 var socket = io();
-
-
 socket.on("showScore",function(score){
-  console.log("Player 1: "+score.player1);
-  console.log("Player 2: "+score.player2);
-
-  var scoreBoard = document.querySelector('.scoreBoard');
-  scoreBoard.innerText = score.player1 + " : " + score.player2;
+    var playerOne = document.querySelector('#PlayerOne');
+    var playerTwo = document.querySelector('#PlayerTwo');
+    playerOne.innerText = '0' + score.player1;
+    playerTwo.innerText = '0' + score.player2;
 });
